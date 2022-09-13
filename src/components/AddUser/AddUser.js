d0b4d6a43fe1e02af634b51b66fb22bc8396e196
mdasif8393 <md.asif.8393@gmail.com>
2022-09-13 20:33:20 +0600

    add crud cr

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const AddUser = () => {
+  const addUsertoDatabase = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+
+    //send data to server
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert("Add a user to database");
+          e.target.reset();
+        }
+      });
+  };
+  return (
+    <div>
+      <form onSubmit={addUsertoDatabase}>
+        <input type="text" name="name" id="" placeholder="name" required />
+        <input type="email" name="email" id="" placeholder="email" required />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
